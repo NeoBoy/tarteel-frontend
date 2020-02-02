55653abf1dab42c5c1cf5cb6a3316e65a5f2530b
Abdellatif Abdelfattah <abdellatifabdelfattah@Abdellatifs-MacBook-Pro.local>
2020-02-02 08:36:49 +0100

    Add component for social media links

---
+++ src/components/SocialLinksFooter.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function SocialLinksFooter() {
+  return (
+    <SocialLinksContainer>
+      <a href="https://www.facebook.com/tarteel.io/" target="_blank">
+        Facebook
+      </a>
+      {' '}·{' '}
+      <a href="https://twitter.com/Tarteel_io" target="_blank">
+        Twitter
+      </a>
+      {' '}·{' '}<a href="https://www.instagram.com/tarteel.io/" target="_blank">Instagram</a>
+    </SocialLinksContainer>
+  );
+}
+
+const SocialLinksContainer = styled.div`
+  color: #c7c7c7;
+  font-size: 14px;
+  width: 100%;
+
+  > a {
+    text-decoration: none;
+    color: #c7c7c7 !important;
+  }
+
+  @media screen and (min-width: ${props => props.theme.breakpoints.md}px) {
+    text-align: right;
+    margin-top: -28px;
+  }
+`;
